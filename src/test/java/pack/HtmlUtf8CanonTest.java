f0757fe163bf2876ce02bb49a0a9703ce891b144
Riskó Péter <Risko.Peter@nisz.hu>
2018-03-08 09:49:08 +0100

    commit

---
+++ src/test/java/pack/HtmlUtf8CanonTest.java
@@ -0,0 +1,31 @@
+package pack;
+
+import org.apache.commons.text.StringEscapeUtils;
+import org.jsoup.Jsoup;
+import org.junit.Test;
+
+public class HtmlUtf8CanonTest {
+
+    /**
+     * goal is to replace HTML entities with UTF-8 characters where possible
+     * (in other words, replace all entities *except* &lt; &gt; &amp; &quot; &apos;)
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void test() throws Exception {
+
+        String html = 
+                "<html><head><META http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">" +
+                "</head><body>&aacute;rv&iacute;zt&#x0171;r&#x0151; &lt;</body></html>";
+
+        // this is not good, keeps only the text content
+        String s = Jsoup.parse(html).text();
+        System.out.println(s);
+        
+        // this is better, but it unescapes the &lt; which is not what I want
+        String s2 = StringEscapeUtils.unescapeHtml4(html);
+        System.out.println("s2: " + s2);
+    }
+
+}
